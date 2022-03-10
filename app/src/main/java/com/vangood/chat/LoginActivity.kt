56f5a886a2360e63221274921b8e0f431cc5512e
Class <a90023y@gmail.com>
2022-03-10 18:06:53 +0800

    login can run

--- app/src/main/java/com/vangood/chat/LoginActivity.kt
+++ app/src/main/java/com/vangood/chat/LoginActivity.kt
@@ -1,11 +1,40 @@
 package com.vangood.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.vangood.chat.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding:ActivityLoginBinding
+    val remember = false
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        val username = binding.tnName.text.toString()
+        val password = binding.tnPassword.text.toString()
+        binding.bLogin.setOnClickListener {
+            if (username == "jack" && password == "1234"){
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                AlertDialog.Builder(this)
+                    .setTitle("Login Fail")
+                    .setMessage("would you like to try again?")
+                    .setPositiveButton("OK"){d,w ->
+                        binding.tnName.setText("")
+                        binding.tnPassword.setText("")
+                    }
+                    .show()
+                }
+        }
+        binding.bSign.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/vangood/chat/MainActivity.kt
+++ app/src/main/java/com/vangood/chat/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.vangood.chat
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
 import com.vangood.chat.databinding.ActivityMainBinding
 import java.util.zip.Inflater
 
@@ -11,6 +14,34 @@ class MainActivity : AppCompatActivity() {
         binding  = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupBottom()
 
+    }
+
+    fun setupBottom(){
+        binding.bHome.setOnClickListener {
+
+        }
+        binding.bSearch.setOnClickListener {
+            val intents = Intent(this,SearchActivity::class.java)
+            startActivity(intents)
+
+        }
+        binding.bId.setOnClickListener {
+            val intenth = Intent(this,LoginActivity::class.java)
+            startActivity(intenth)
+        }
+
+
+    }
+
+}
+class NameContract:ActivityResultContract<Float,String>(){
+    override fun createIntent(context: Context, input: Float?): Intent {
+        TODO("Not yet implemented")
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String {
+        TODO("Not yet implemented")
     }
 }
